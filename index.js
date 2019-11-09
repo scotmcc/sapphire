eac7e564011f487d5241d43840d9a1078b313259
Scot McConnaughay <scotmcc@gmail.com>
2019-11-08 20:35:36 -0700

    feat(global): Added ExpressJS to project
    Added Express from NPM, implemented simple web server, and created a few
    simple HTML pages to be served from a public 'www' directory.

--- index.js
+++ index.js
@@ -1,2 +1,15 @@
 require('dotenv').config();
 
+const path = require('path');
+const express = require('express');
+const app = express();
+
+app.use(express.static('www'));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.resolve(__dirname, './www/404.html'));
+});
+
+app.listen(process.env.PORT, () => {
+  console.log('Application listening on: ' + process.env.PORT);
+})
