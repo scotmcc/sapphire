dfe8b05526bf35a0d35773a46e0c83326f8fca67
Scot McConnaughay <scotmcc@gmail.com>
2019-11-08 20:45:04 -0700

    Added CSS and basic javascript

--- www/js/index.js
+++ www/js/index.js
@@ -1,1 +1,9 @@
 console.log('Welcome to the world!')
+
+var message = document.getElementById('message');
+var send = document.getElementById('send');
+var response = document.getElementById('response');
+
+send.addEventListener('mousedown', (e) => {
+  response.value = message.value;
+});
